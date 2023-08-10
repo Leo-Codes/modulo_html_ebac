f1b584e4f57eae306e825a08ab4c735542f3d0df
Leo-Codes <leo.ccodes@gmail.com>
2023-08-09 22:37:47 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function validar(A, B){
+
+
+
+
+}
+
+function setup(a_id, b_id){
+
+    document.getElementById(a_id).addEventListener('change',()=>{
+        console.log('mudou');
+    })
+
+    document.getElementById(b_id).addEventListener('change',()=>{
+        console.log('mudou');
+    })
+
+
+}
